73a647477180ca094448c2dd546b80b7fea0b64b
Arnaud GENTY <arnaud.genty@gmail.com>
2019-02-02 21:57:44 +0100

    Ca complie

--- src/module.js
+++ src/module.js
@@ -1,4 +1,16 @@
 import { PanelCtrl } from 'grafana/app/plugins/sdk'; // will be resolved to app/plugins/sdk
+var mxgraph = require("../node_modules/mxgraph/javascript/dist/build")({
+      mxImageBasePath: "../node_modules/mxgraph/javascript/src/images",
+      mxBasePath: "../node_modules/mxgraph/javascript/src"
+    }),
+    mxGraph = mxgraph.mxGraph,
+    mxShape = mxgraph.mxShape,
+    mxConnectionConstraint = mxgraph.mxConnectionConstraint,
+    mxPoint = mxgraph.mxPoint,
+    mxPolyline = mxgraph.mxPolyline,
+    mxEvent = mxgraph.mxEvent,
+    mxRubberband = mxgraph.mxRubberband,
+    mxCellState = mxgraph.mxCellState;
 
 class Ctrl extends PanelCtrl {
 
@@ -13,6 +25,10 @@ class Ctrl extends PanelCtrl {
   initStyles() {
     window.System.import(this.panelPath + 'css/flowchart.css!');
     // Remove next lines if you don't need separate styles for light and dark themes
+  }
+
+  initMxGraph() {
+    
   }
   
   get panelPath() {
